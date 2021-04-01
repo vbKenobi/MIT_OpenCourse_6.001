34304990ed77ae1caeb014708bf4f730e2f7ca6a
Yajn <yajnvb@gmail.com>
2021-03-31 22:50:50 -0700

    Initial Commit added a test file

---
+++ test.py
@@ -0,0 +1,7 @@
+#This is just a test to see whether or not the Github has been properly linked, and making sure python was properly installed
+
+import numpy as np
+
+print("Hello World")
+
+print(np.sin(3.1415/6))
